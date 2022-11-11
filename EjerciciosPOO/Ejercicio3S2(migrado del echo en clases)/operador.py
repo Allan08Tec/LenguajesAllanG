ea79376a718776bf4fbbf2d955c13e41825ce68e
Allan08Tec <61554258+Allan08Tec@users.noreply.github.com>
2022-11-11 11:38:14 -0600

    Create operador.py

---
+++ EjerciciosPOO/Ejercicio3S2(migrado del echo en clases)/operador.py
@@ -0,0 +1,18 @@
+#crear la clase operador con 1 variable operador
+
+class Operador:
+    
+        def __init__(self, operador):
+            self.operador = operador    
+
+
+#crear los metodos get y set
+
+        def getOperador(self):
+            return self.operador
+    
+        def setOperador(self, operador):
+            self.operador = operador
+
+        
+
