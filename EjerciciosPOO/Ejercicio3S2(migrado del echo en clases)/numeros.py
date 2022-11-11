0eadecdb6504a4b36f37c78cb677d8d2b7d4d927
Allan08Tec <61554258+Allan08Tec@users.noreply.github.com>
2022-11-11 11:34:50 -0600

    Create numeros.py

---
+++ EjerciciosPOO/Ejercicio3S2(migrado del echo en clases)/numeros.py
@@ -0,0 +1,15 @@
+#crear una clase numero con a variable num 
+class Numero:
+
+    def __init__(self, num):
+        self.num = num
+    
+#crear el metodos get y set
+
+
+    def getNum(self):
+        return self.num
+
+    def setNum(self, num):
+        self.num = num
+
